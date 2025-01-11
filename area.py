d0baa7a983a70799cec4999c06a85b0bf9b97f2d
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-01-11 14:39:08 -0300

    add area.py

---
+++ area.py
@@ -0,0 +1,13 @@
+PI = 3.14159
+
+arquivo = "3.0 4.0 5.2"
+
+a = float(arquivo.split(" ")[0])
+b = float(arquivo.split(" ")[1])
+c = float(arquivo.split(" ")[2])
+
+print(f'TRIANGULO: {((a * c) / 2):.3f}')
+print(f'CIRCULO: {PI*c*c:.3f}')
+print(f'TRAPEZIO: {((a+b)*c) / 2:.3f}')
+print(f'QUADRADO: {(b*b):.3f}')
+print(f'RETANGULO: {a*b:.3f}')
