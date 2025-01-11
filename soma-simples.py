c3380655cbf445a45e28f3143338add222c6f989
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-01-11 16:46:58 -0300

    removendo espaços em brancos

--- soma-simples.py
+++ soma-simples.py
@@ -1,6 +1,6 @@
 arquivo = "30 \n 10"
 
-primeiro_valor = int(arquivo.split("\n")[0])
-segundo_valor = int(arquivo.split("\n")[1])
+primeiro_valor = int(arquivo.split("\n")[0].strip())
+segundo_valor = int(arquivo.split("\n")[1].strip())
 
 print(f'SOMA = {primeiro_valor + segundo_valor}')
