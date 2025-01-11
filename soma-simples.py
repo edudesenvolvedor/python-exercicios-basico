24d335c34530565927fb9c6b9c1c9a56c31c4603
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-01-11 14:30:23 -0300

    Alterado para entrada de dados tipo arquivo

--- soma-simples.py
+++ soma-simples.py
@@ -1,4 +1,6 @@
-primeiro_valor = int(input('Digite o primeiro valor: '))
-segundo_valor = int(input('Digite o segundo valor: '))
+arquivo = "30 \n 10"
+
+primeiro_valor = int(arquivo.split("\n")[0])
+segundo_valor = int(arquivo.split("\n")[1])
 
 print(f'SOMA = {primeiro_valor + segundo_valor}')
