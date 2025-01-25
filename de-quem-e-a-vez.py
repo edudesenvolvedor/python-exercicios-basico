c40ec1f2d1b54f6f711ce67015e16e0bfa6b362f
Eduardo A. Pacheco <82110822+edudesenvolvedor@users.noreply.github.com>
2025-01-25 10:27:17 -0300

    Update de-quem-e-a-vez.py

--- de-quem-e-a-vez.py
+++ de-quem-e-a-vez.py
@@ -1,11 +1,24 @@
-expediente = input()
-tempos_producao = input()
+def determinar_vencedor(rodada):
 
-tempo_total_producao = 0
-for tempo in tempos_producao.split(" "):
-    tempo_total_producao += float(tempo)
+    resultado = 'IMPAR' if (rodada['N'] + rodada['M']) % 2 else 'PAR'
+    return rodada['jogador1'] if rodada['jogada1'] == resultado else rodada['jogador2']
 
-if tempo_total_producao > float(expediente):
-    print("Deixa para amanha!")
-else:
-    print("Farei hoje!")
+QT = int(input())
+
+rodadas = []
+
+for i in range(QT):
+    jogador1, jogada1, jogador2, jogada2 = input().strip().split(' ')
+    N, M = map(int, input().strip().split(' '))
+    rodadas.append({
+        'jogador1': jogador1,
+        'jogada1': jogada1,
+        'jogador2': jogador2,
+        'jogada2': jogada2,
+        'N': N,
+        'M': M
+    })
+
+for i, rodada in enumerate(rodadas, start=1):
+    vencedor = determinar_vencedor(rodada)
+    print(f"{vencedor}")
