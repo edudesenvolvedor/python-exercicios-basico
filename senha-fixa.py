783dfe4ccb2c2948051108d66e39637d86a2c416
Eduardo A. Pacheco <82110822+edudesenvolvedor@users.noreply.github.com>
2025-01-25 10:28:11 -0300

    Update senha-fixa.py

--- senha-fixa.py
+++ senha-fixa.py
@@ -1,13 +1,11 @@
-arquivo = "2200\n1020\n2022\n2002"
+while True:
+    try:
+        senha = int(input())
 
-def valida_senha(senha):
-    if senha == 2002:
-        print("Acesso Permitido")
-    else:
+        if(senha == 2002):
+            print("Acesso Permitido")
+            break
+
         print("Senha Invalida")
-
-lista_senhas = arquivo.splitlines()
-for senha in lista_senhas:
-    senha = int(senha.strip())
-    valida_senha(senha)
-
+    except EOFError:
+        break
